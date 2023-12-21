e261cdd312924c94b5707e37ea08105470d7b2b4
PedroPortela29 <phaporte@gmail.com>
2023-12-21 19:38:41 -0300

    Add project files.

---
+++ CalculadoraIMC/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Drawing;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+float resultadoIMC(float kg, float altura)
+{
+
+    float alturaAoQuadrado = altura * altura;
+
+
+    return kg / alturaAoQuadrado;
+}
+    Console.WriteLine("Digite seu peso: ");
+float kg = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite sua altura (em metros): ");
+float altura= float.Parse(Console.ReadLine());
+if (altura == 0)
+{
+    Console.WriteLine("A altura deve ser maior que zero");
+}
+
+else 
+{
+    float imc = resultadoIMC(kg, altura);
+    string categoria = ClassificarIMC(imc);
+
+    Console.WriteLine($"Seu IMC: {imc:F2} - Categoria: {categoria}");
+}
+
+string ClassificarIMC(float imc) 
+{ 
+
+    float magrezaLimite = 18.5f;
+    float normalLimte = 24.9f;
+    float sobrepesoLimite = 29.9f;
+    float obesidadeLimite = 30.0f;
+
+    if (imc < magrezaLimite)
+    {
+        return "Magreza";
+    }
+    else if (imc <= normalLimte)
+    {
+        return "Normal";
+    }
+    else if (imc <= sobrepesoLimite)
+    {
+       return "Sobrepeso";
+    }
+    else
+    {
+        return "Obesidade";
+    }
+}
